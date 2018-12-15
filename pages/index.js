bcd282ffbf37da0152d30025b4583d07e4e0427f
vitalyliber <zenamax@gmail.com>
2018-12-15 20:20:07 +0300

    icons were added

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,14 @@ export default () => (
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
+      {/*thanks to the icon generator https://realfavicongenerator.net*/}
+      <link rel="apple-touch-icon" sizes="180x180" href="../static/favicon/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="../static/favicon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon/favicon-16x16.png"/>
+      <link rel="manifest" href="../static/favicon/site.webmanifest"/>
+      <link rel="mask-icon" href="../static/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#ffffff"/>
     </Head>
     <GeneralScreen/>
 
